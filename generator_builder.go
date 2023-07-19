6d33aa52196ff0996057b05506fecb611bb5161b
郭延宝 <gyb@strongsoft.net>
2023-07-19 11:07:15 +0800

    update

--- generator_builder.go
+++ generator_builder.go
@@ -51,41 +51,55 @@ func (g *Generator) GetModels() map[string]*generate.QueryStructMeta {
 }
 
 // BelongsTo
-func (g *Generator) BelongsTo(key string, tableName string) model.CreateFieldOpt {
-	return FieldRelate(field.BelongsTo, key, g.BuildModel(tableName), &field.RelateConfig{
+func (g *Generator) BelongsTo(fieldName string, tableName string) model.CreateFieldOpt {
+	return FieldRelate(field.BelongsTo, fieldName, g.BuildModel(tableName), &field.RelateConfig{
 		RelatePointer: true,
 		GORMTag:       fmt.Sprintf("foreignKey:%s_id", tableName),
 	})
 }
 
 // HasOne
-func (g *Generator) HasOne(key string, tableName string) model.CreateFieldOpt {
-	return FieldRelate(field.HasOne, key, g.BuildModel(tableName), &field.RelateConfig{
+func (g *Generator) HasOne(fieldName string, tableName string) model.CreateFieldOpt {
+	return FieldRelate(field.HasOne, fieldName, g.BuildModel(tableName), &field.RelateConfig{
 		RelatePointer: true,
 		GORMTag:       "foreignKey:id",
 	})
 }
 
 // HasMany
-func (g *Generator) HasMany(key string, tableName string) model.CreateFieldOpt {
-	return FieldRelate(field.HasMany, key, g.BuildModel(tableName), &field.RelateConfig{
+func (g *Generator) HasMany(fieldName string, tableName string) model.CreateFieldOpt {
+	return FieldRelate(field.HasMany, fieldName, g.BuildModel(tableName), &field.RelateConfig{
 		RelateSlicePointer: true,
 		GORMTag:            "foreignKey:id",
 	})
 }
 
 // HasTree 用于一对多的关联配置，以pid为外键进行关联，构建成 {items:[{items:...,nodes:[]}],nodes:[]}
-func (g *Generator) HasTree(key string, tableName string) model.CreateFieldOpt {
-	return FieldRelate(field.HasMany, key, g.BuildModel(tableName), &field.RelateConfig{
+func (g *Generator) HasTree(fieldName string, tableName string) model.CreateFieldOpt {
+	return FieldRelate(field.HasMany, fieldName, g.BuildModel(tableName), &field.RelateConfig{
 		RelateSlicePointer: true,
 		GORMTag:            "foreignKey:pid;references:id",
 	})
 }
 
 // HasTreeByParentID
-func (g *Generator) HasTreeByParentID(key string, tableName string) model.CreateFieldOpt {
-	return FieldRelate(field.HasMany, key, g.BuildModel(tableName), &field.RelateConfig{
+func (g *Generator) HasTreeByParentID(fieldName string, tableName string) model.CreateFieldOpt {
+	return FieldRelate(field.HasMany, fieldName, g.BuildModel(tableName), &field.RelateConfig{
 		RelateSlicePointer: true,
 		GORMTag:            "foreignKey:parent_id;references:id",
 	})
+}
+
+// HasData
+func (g *Generator) HasData(fieldName string, typeName string) model.CreateFieldOpt {
+	return func(*model.Field) *model.Field {
+		return &model.Field{
+			Name:    fieldName,
+			Type:    typeName,
+			JSONTag: ns.ColumnName("", fieldName),
+			GORMTag: "-",
+			// NewTag:       config.NewTag,
+			// OverwriteTag: config.OverwriteTag,
+		}
+	}
 }
