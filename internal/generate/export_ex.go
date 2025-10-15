f393c99e99c3fa0a2ef0462eab73676276cdfe10
mocheer <mocheer@foxmail.com>
2025-10-15 09:45:47 +0800

    update

--- internal/generate/export_ex.go
+++ internal/generate/export_ex.go
@@ -1,6 +1,28 @@
 package generate
 
+import (
+	"fmt"
+
+	"gorm.io/gen/internal/parser"
+)
+
 // AddMethod
 func (g *QueryStructMeta) AddMethod(method interface{}) {
 	g.addMethodFromAddMethodOpt(method)
+}
+
+func (g *QueryStructMeta) AddMethodWidthMapTest(value map[string]any) {
+	method := &parser.Method{
+		Receiver:   parser.Param{IsPointer: true, Type: g.ModelStructName},
+		MethodName: "ToSchemaBuffer",
+		Doc:        "",
+		Params:     []parser.Param{},
+		Result: []parser.Param{
+			{
+				Type: "[]byte",
+			},
+		},
+		Body: fmt.Sprintf("{\n\treturn %s\n} ", value["body"].(string)),
+	}
+	g.ModelMethods = append(g.ModelMethods, method)
 }
